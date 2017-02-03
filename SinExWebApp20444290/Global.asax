7952b2ff106b40fbc0670a4240091c9fce3feb9a
ejloriot <ejloriot@CSL4WK36>
2017-02-03 10:15:53 +0800

    Add project files.

---
+++ SinExWebApp20444290/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SinExWebApp20444290.MvcApplication" Language="C#" %>
---
+++ SinExWebApp20444290/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SinExWebApp20444290.Startup))]
+namespace SinExWebApp20444290
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
