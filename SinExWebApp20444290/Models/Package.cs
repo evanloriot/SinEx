1090e8405642a318fe3f193042454e8856ec3f5d
tfmgavrel <tfmgavrel@CSL4WK27>
2017-05-02 17:27:55 +0800

    Add variable req.5 package model

--- SinExWebApp20444290/Models/Package.cs
+++ SinExWebApp20444290/Models/Package.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SinExWebApp20444290.Models
 {
+    [Table("Package")]
     public class Package
     {
         [Display(Name = "Package Shipping ID")]
