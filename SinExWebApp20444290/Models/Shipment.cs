fb67f2fd93af14c7336cdf1b49622444e7e18602
ejloriot <evan.loriot@gmail.com>
2017-05-04 20:53:42 +0800

    Adding Payed field to Shipment for Invoice tracking

--- SinExWebApp20444290/Models/Shipment.cs
+++ SinExWebApp20444290/Models/Shipment.cs
@@ -55,6 +55,7 @@ namespace SinExWebApp20444290.Models
         public virtual bool ShipmentPayer { get; set; }
         public virtual decimal? Duty { get; set; }
         public virtual decimal? Tax { get; set; }
+        public virtual bool Payed { get; set; }
         [Display(Name = "Actual total shipment cost")]
         public virtual decimal ShipmentAmount { get; set; }
         //[Display(Name = "Shipment confirmed")]
